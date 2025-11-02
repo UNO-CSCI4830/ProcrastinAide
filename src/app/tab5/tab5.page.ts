f1479c98fa0d5a0f93de9a62e0229d63144d976d
Treh06 <Twahreh10@gmail.com>
2025-11-01 20:57:46 -0500

    feat: added task list UI,

---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonNote,
+  IonCheckbox,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TaskService, TaskModel } from '../services/task.service';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss'],
+  imports: [CommonModule, RouterModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonNote, IonCheckbox, IonButton],
+})
+export class Tab5Page {
+  tasks$: Observable<TaskModel[]>;
+  todo$: Observable<TaskModel[]>;
+  completed$: Observable<TaskModel[]>;
+
+  constructor(private taskService: TaskService) {
+    this.tasks$ = this.taskService.tasks$;
+    this.todo$ = this.tasks$.pipe(map((list) => list.filter((t) => !t.completed)));
+    this.completed$ = this.tasks$.pipe(map((list) => list.filter((t) => t.completed)));
+  }
+
+  toggleCompleted(t: TaskModel, value: boolean) {
+    this.taskService.setCompleted(t.createdAt, value);
+  }
+}
