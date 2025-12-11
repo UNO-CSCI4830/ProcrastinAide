337defadc3d52a692b1bed8e163018b094471b54
Ryan Dilley <ryandilley54@outlook.com>
2025-12-11 11:39:35 -0600

    Pomodoro Timer
    Added a timer function that works with break sessions as well as study sessions to prompt user when to study and when not to on app.

--- src/app/pages/pomodoro/pomodoro.page.ts
+++ src/app/pages/pomodoro/pomodoro.page.ts
@@ -1,38 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
   IonButton,
-  IonFab,
-  IonFabButton,
-  IonIcon
+  IonBackButton,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonSelect,
+  IonSelectOption
 } from '@ionic/angular/standalone';
+
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'pomodoro',
-  templateUrl: 'pomodoro.page.html',
-  styleUrls: ['pomodoro.page.scss'],
+  standalone: true,
+  templateUrl: './pomodoro.page.html',
+  styleUrls: ['./pomodoro.page.scss'],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule,
+
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
     IonButton,
-    IonFab,
-    IonFabButton,
-    IonIcon
+    IonBackButton,
+    IonItem,
+    IonLabel,
+    IonIcon,
+    IonSelect,
+    IonSelectOption
   ],
 })
-export class PomodoroPage {
-  
-  constructor() {
-    
+export class PomodoroPage implements OnDestroy {
+
+  selectedFocusMinutes: number = 15;   // user study duration
+  breakMinutes: number = 2;            // user break duration (default)
+
+  timeLeft: number = 0;
+  timer: any = null;
+
+  isFocusSession: boolean = true;
+  isPaused: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 
+  startFocusTimer() {
+    if (this.timer) clearInterval(this.timer);
+
+    this.isFocusSession = true;
+    this.isPaused = false;
+    this.timeLeft = this.selectedFocusMinutes * 60;
+
+    this.timer = setInterval(() => {
+      if (!this.isPaused) {
+        this.timeLeft--;
+      }
+
+      if (this.timeLeft <= 0) {
+        clearInterval(this.timer);
+        alert("Break time!");
+        this.startBreakTimer();
+      }
+    }, 1000);
+  }
+
+  pauseTimer() {
+    this.isPaused = !this.isPaused;
+  }
+
+  startBreakTimer() {
+    if (this.timer) clearInterval(this.timer);
+
+    this.isFocusSession = false;
+    this.isPaused = false;
+    this.timeLeft = this.breakMinutes * 60;
+
+   this.router.navigate(['/trivia-test'], {
+  state: { breakTime: this.breakMinutes * 60 }
+});
+
+
+    this.timer = setInterval(() => {
+      if (!this.isPaused) {
+        this.timeLeft--;
+      }
+
+      if (this.timeLeft <= 0) {
+        clearInterval(this.timer);
+        alert("Break time over!");
+        this.router.navigate(['/pomodoro']);
+      }
+    }, 1000);
+  }
+
+  get minutes(): string {
+    return Math.floor(this.timeLeft / 60).toString();
+  }
+
+  get seconds(): string {
+    const s = this.timeLeft % 60;
+    return s < 10 ? '0' + s : s.toString();
+  }
 }
